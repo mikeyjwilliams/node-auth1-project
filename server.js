a17a3e519ae9f008b4df332a7f022d8e5fe54297
Michael Williams <manicdesigner@gmail.com>
2020-03-11 16:13:13 -0400

    add route for auth-router / check api route, 404 route 500 error

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const userRouter = require('./auth/auth-router');
 const router = express();
+
+router.use('/api', userRouter);
+
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'welcome to users API' });
+});
+
+router.use((req, res) => {
+  res.status(404).json({ message: '404 page not found' });
+});
+
+router.use((err, req, res, next) => {
+  res.status(500).json({ message: 'internal server error' });
+});
 
 module.exports = router;
