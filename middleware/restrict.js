dfb6d761b4e5cba24be4401682433c3414bd7553
Mikey Williams <manicdesigner@gmail.com>
2020-03-11 20:49:54 -0400

    add a middleware file to restrict users that dont have there password in db

---
+++ middleware/restrict.js
@@ -0,0 +1,30 @@
+const Users = require('../users/users-model');
+const bcrypt = require('bcryptjs');
+
+function restrict() {
+	const authErr = { message: 'invalid credentials' };
+	return async (req, res, next) => {
+		const { username, password } = req.headers;
+		if (!username || !password) {
+			res.status(401).json(authErr);
+		}
+		try {
+			const userCheck = Users.findBy({ username }).first();
+
+			if (!userCheck) {
+				res.status(401).json(authErr);
+			}
+			const passwordCheck = bcrypt.compare(password, userCheck.password);
+
+			if (!passwordCheck) {
+				res.status(401).json(authErr);
+			}
+			next();
+		} catch (err) {
+			console.log(err);
+			next(err);
+		}
+	};
+}
+
+module.exports = restrict;
