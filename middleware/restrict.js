629d5de5ef81f9dda4ff8653be5da9c76c1dfe3e
Michael Williams <manicdesigner@gmail.com>
2020-03-12 21:59:30 -0400

    reduced code looking for session data from req.session

--- middleware/restrict.js
+++ middleware/restrict.js
@@ -1,30 +1,19 @@
-const Users = require('../users/users-model');
-const bcrypt = require('bcryptjs');
+// const Users = require('../users/users-model');
+// const bcrypt = require('bcryptjs');
 
 function restrict() {
-	const authErr = { message: 'invalid credentials' };
-	return async (req, res, next) => {
-		const { username, password } = req.headers;
-		if (!username || !password) {
-			res.status(401).json(authErr);
-		}
-		try {
-			const userCheck = await Users.findBy({ username }).first();
-
-			if (!userCheck) {
-				res.status(401).json(authErr);
-			}
-			const passwordCheck = await bcrypt.compare(password, userCheck.password);
-
-			if (!passwordCheck) {
-				res.status(401).json(authErr);
-			}
-			next();
-		} catch (err) {
-			console.log(err);
-			next(err);
-		}
-	};
+  const authErr = { message: 'invalid credentials' };
+  return async (req, res, next) => {
+    try {
+      if (!res.session || !req.session.user) {
+        return res.status(401).json(authErr);
+      }
+      next();
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  };
 }
 
 module.exports = restrict;
